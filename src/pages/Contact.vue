59d586108d0c9492d0e0281179fc7a85e1a0f45a
Akira Kanno <teinen.github@gmail.com>
2018-11-08 07:12:07 +0900

    Add Contact Page

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,9 +1,24 @@
 <template>
-  <section id="career">
+  <section id="about">
     <div class="card">
-      <div class="sorry">
-        Oops!! Sorry, this page is developing now!
-      </div>
+      <!-- Page title -->
+      <PageHeadingComponent contentProp="Contact" />
+
     </div>
   </section>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import PageHeading from "@/components/atomic/PageHeading.vue";
+
+@Component({
+  components: {
+    PageHeadingComponent: PageHeading
+  }
+})
+export default class Contact extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+</style>
